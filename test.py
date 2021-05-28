3e27042808e5f8bc4f960bf9abb509535b8e176c
jorgetoledo23 <jorge.toledo.6@hotmail.com>
2021-05-28 14:37:38 -0400

    asd

---
+++ test.py
@@ -0,0 +1,6 @@
+#string -> cadena de texto / literal <class 'str'>
+nombre = "Jorge Toledo"
+print(type(nombre))
+apellido = "Toledo"
+direccion = "Merced 750 Curico"
+telefono = "+569 1234 1234"
---
+++ tiposDatos.py
@@ -0,0 +1,23 @@
+# int -> Integer = Entero  <class 'int'>
+edad = 10
+print(type(edad))
+precio = 99990
+cantHijos = 2
+cantProductos = 10
+
+edad = int(input("Ingrese su Edad: "))
+print(type(edad))
+
+if edad >= 18:
+    print("Mayor de Edad")
+
+
+#string -> cadena de texto / literal <class 'str'>
+nombre = "Jorge Toledo"
+print(type(nombre))
+apellido = "Toledo"
+direccion = "Merced 750 Curico"
+telefono = "+569 1234 1234"
+producto = "Logitech G502 Wireless"
+
+
